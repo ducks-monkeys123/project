3552eb2a55a3a1bf2757a6db69baa4cb32aab2ba
ducks-monkeys123 <rhanerojan@gmail.com>
2025-05-02 23:54:35 +1200

    Updates

---
+++ server/index.js
@@ -0,0 +1,209 @@
+import { jsx, jsxs } from "react/jsx-runtime";
+import { PassThrough } from "node:stream";
+import { createReadableStreamFromReadable } from "@react-router/node";
+import { ServerRouter, useMatches, useActionData, useLoaderData, useParams, useRouteError, Meta, Links, ScrollRestoration, Scripts, isRouteErrorResponse } from "react-router";
+import { isbot } from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+import { createElement } from "react";
+const streamTimeout = 5e3;
+function handleRequest(request, responseStatusCode, responseHeaders, routerContext, loadContext) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = false;
+    let userAgent = request.headers.get("user-agent");
+    let readyOption = userAgent && isbot(userAgent) || routerContext.isSpaMode ? "onAllReady" : "onShellReady";
+    const { pipe, abort } = renderToPipeableStream(
+      /* @__PURE__ */ jsx(ServerRouter, { context: routerContext, url: request.url }),
+      {
+        [readyOption]() {
+          shellRendered = true;
+          const body = new PassThrough();
+          const stream = createReadableStreamFromReadable(body);
+          responseHeaders.set("Content-Type", "text/html");
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode
+            })
+          );
+          pipe(body);
+        },
+        onShellError(error) {
+          reject(error);
+        },
+        onError(error) {
+          responseStatusCode = 500;
+          if (shellRendered) {
+            console.error(error);
+          }
+        }
+      }
+    );
+    setTimeout(abort, streamTimeout + 1e3);
+  });
+}
+const entryServer = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: handleRequest,
+  streamTimeout
+}, Symbol.toStringTag, { value: "Module" }));
+function withComponentProps(Component) {
+  return function Wrapped() {
+    const props = {
+      params: useParams(),
+      loaderData: useLoaderData(),
+      actionData: useActionData(),
+      matches: useMatches()
+    };
+    return createElement(Component, props);
+  };
+}
+function withErrorBoundaryProps(ErrorBoundary3) {
+  return function Wrapped() {
+    const props = {
+      params: useParams(),
+      loaderData: useLoaderData(),
+      actionData: useActionData(),
+      error: useRouteError()
+    };
+    return createElement(ErrorBoundary3, props);
+  };
+}
+const links = () => [{
+  rel: "preconnect",
+  href: "https://fonts.googleapis.com"
+}, {
+  rel: "preconnect",
+  href: "https://fonts.gstatic.com",
+  crossOrigin: "anonymous"
+}, {
+  rel: "stylesheet",
+  href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+}];
+function Layout({
+  children
+}) {
+  return /* @__PURE__ */ jsxs("html", {
+    lang: "en",
+    children: [/* @__PURE__ */ jsxs("head", {
+      children: [/* @__PURE__ */ jsx("meta", {
+        charSet: "utf-8"
+      }), /* @__PURE__ */ jsx("meta", {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      }), /* @__PURE__ */ jsx(Meta, {}), /* @__PURE__ */ jsx(Links, {})]
+    }), /* @__PURE__ */ jsxs("body", {
+      children: [children, /* @__PURE__ */ jsx(ScrollRestoration, {}), /* @__PURE__ */ jsx(Scripts, {})]
+    })]
+  });
+}
+const root = withComponentProps(function App() {
+  return /* @__PURE__ */ jsx("div", {
+    children: /* @__PURE__ */ jsx("h1", {
+      children: "Hello there!"
+    })
+  });
+});
+const ErrorBoundary = withErrorBoundaryProps(function ErrorBoundary2({
+  error
+}) {
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack;
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? "404" : "Error";
+    details = error.status === 404 ? "The requested page could not be found." : error.statusText || details;
+  }
+  return /* @__PURE__ */ jsxs("main", {
+    className: "pt-16 p-4 container mx-auto",
+    children: [/* @__PURE__ */ jsx("h1", {
+      children: message
+    }), /* @__PURE__ */ jsx("p", {
+      children: details
+    }), stack]
+  });
+});
+const route0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  ErrorBoundary,
+  Layout,
+  default: root,
+  links
+}, Symbol.toStringTag, { value: "Module" }));
+function meta({}) {
+  return [{
+    title: "New React Router App"
+  }, {
+    name: "description",
+    content: "Welcome to React Router!"
+  }];
+}
+const home = withComponentProps(function HomePage() {
+  return /* @__PURE__ */ jsx("div", {
+    children: /* @__PURE__ */ jsx("h1", {
+      children: "Home"
+    })
+  });
+});
+const route1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: home,
+  meta
+}, Symbol.toStringTag, { value: "Module" }));
+const scan = withComponentProps(function ScanPage() {
+  return /* @__PURE__ */ jsx("div", {
+    children: /* @__PURE__ */ jsx("h1", {
+      children: "Scan QR code"
+    })
+  });
+});
+const route2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: scan
+}, Symbol.toStringTag, { value: "Module" }));
+const serverManifest = { "entry": { "module": "/assets/entry.client-DQKr6M1f.js", "imports": ["/assets/chunk-AYJ5UCUI-CDxOV4f-.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": true, "module": "/assets/root-DYL8nopm.js", "imports": ["/assets/chunk-AYJ5UCUI-CDxOV4f-.js", "/assets/with-props-CBXwCruZ.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/home": { "id": "routes/home", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/assets/home-YB3C2bzS.js", "imports": ["/assets/with-props-CBXwCruZ.js", "/assets/chunk-AYJ5UCUI-CDxOV4f-.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/scan": { "id": "routes/scan", "parentId": "root", "path": "project/scan", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/assets/scan-CajocEDO.js", "imports": ["/assets/with-props-CBXwCruZ.js", "/assets/chunk-AYJ5UCUI-CDxOV4f-.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 } }, "url": "/assets/manifest-2f4e3a15.js", "version": "2f4e3a15", "sri": void 0 };
+const assetsBuildDirectory = "build/client";
+const basename = "/";
+const future = { "unstable_middleware": false, "unstable_optimizeDeps": false, "unstable_splitRouteModules": false, "unstable_subResourceIntegrity": false, "unstable_viteEnvironmentApi": false };
+const ssr = true;
+const isSpaMode = false;
+const prerender = [];
+const publicPath = "/";
+const entry = { module: entryServer };
+const routes = {
+  "root": {
+    id: "root",
+    parentId: void 0,
+    path: "",
+    index: void 0,
+    caseSensitive: void 0,
+    module: route0
+  },
+  "routes/home": {
+    id: "routes/home",
+    parentId: "root",
+    path: void 0,
+    index: true,
+    caseSensitive: void 0,
+    module: route1
+  },
+  "routes/scan": {
+    id: "routes/scan",
+    parentId: "root",
+    path: "project/scan",
+    index: void 0,
+    caseSensitive: void 0,
+    module: route2
+  }
+};
+export {
+  serverManifest as assets,
+  assetsBuildDirectory,
+  basename,
+  entry,
+  future,
+  isSpaMode,
+  prerender,
+  publicPath,
+  routes,
+  ssr
+};
